39142d2b5d8e3fe1ab4b19dad426136f48a48001
Jhovanny Canas <jhovanycanas@Woloxs-MacBook-Pro.local>
2019-06-25 11:42:20 -0500

    resolve comments

--- wolox-books/src/app/screens/login/login.component.ts
+++ wolox-books/src/app/screens/login/login.component.ts
@@ -35,9 +35,8 @@ export class LoginComponent implements OnInit {
         password : post.password
       }
     };
-    console.log(JSON.stringify(session));
     this.userservice.login(session)
-    .subscribe(result => 
+    .subscribe(result =>
       console.log(result.access_token),
       error => console.log('error', error));
   }
